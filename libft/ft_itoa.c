5a9a7ede72be2086dd549e146f93b3eadecaa930
Pedro Garcia Fernandez-Valderrama <pedgarci@c1r11s6.42madrid.com>
2023-02-22 21:59:50 +0100

    itoa y split

---
+++ libft/ft_itoa.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pedgarci <pedgarci@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/22 19:10:22 by pedgarci          #+#    #+#             */
+/*   Updated: 2023/02/22 19:10:26 by pedgarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+int int_len(n)
+{
+    unsigned int len;
+    
+    len = 0;
+    
+    if (n == 0)
+        return(1);
+    if (n < 0)
+    {
+        len += 1;
+        n = -n;
+    }
+    while (n != 0)
+    {
+        n = n / 10;
+        len++;
+    }
+    return len;
+}
+
+char *ft_itoa(int n)
+{
+    char *string;
+    unsigned int number;
+    unsigned int len;
+    
+    len = int_len(n);
+
+    if (!(string = (char *)malloc((len + 1) * sizeof(char))))
+        return(0);
+    if (n < 0)
+    {
+        string[0] = '-';
+        number = -n;
+    }
+    else 
+        number = n;
+    if (number == 0)
+        string[0] = '0';
+    string[len] = '\0';
+    while (number != 0)
+    {
+        
+        string[len - 1] = number % 10 + 48;
+        number = number / 10;
+        len --;
+    }
+    
+    return (string);
+}
+/*
+int main (void)
+{
+    int num;
+
+    num = 3435636;
+    printf("el numero %d es el string %s\n", num, ft_itoa(num));
+    return(0);
+}*/
--- libft/ft_split.c
+++ libft/ft_split.c
@@ -11,9 +11,50 @@
 /* ************************************************************************** */
 #include "libft.h"
 
-char	**ft_split(char const *s, char c)
+static int	count_words(const char *str, char c)
 {
-	char **myarray;
+	int i;
+	int trigger;
 
+	i = 0;
+	trigger = 0;
+	while (*str)
+	{
+		if (*str != c && trigger == 0)
+		{
+			trigger = 1;
+			i++;
+		}
+		else if (*str == c)
+			trigger = 0;
+		str++;
+	}
+	return (i);
+}
 
+char		**ft_split(char const *s, char c)
+{
+	size_t	i;
+	size_t	j;
+	int		index;
+	char	**split;
+
+	if (!s || !(split = malloc((count_words(s, c) + 1) * sizeof(char *))))
+		return (0);
+	i = 0;
+	j = 0;
+	index = -1;
+	while (i <= ft_strlen(s))
+	{
+		if (s[i] != c && index < 0)
+			index = i;
+		else if ((s[i] == c || i == ft_strlen(s)) && index >= 0)
+		{
+			split[j++] = ft_substr(s, index, i - index);
+			index = -1;
+		}
+		i++;
+	}
+	split[j] = 0;
+	return (split);
 }
