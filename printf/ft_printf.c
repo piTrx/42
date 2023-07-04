f942f789623d3a88f9687b9fef546eaacb94527f
Pedro Garcia Fernandez-Valderrama <pedgarci@c1r13s5.42madrid.com>
2023-07-04 17:47:53 +0200

    initial

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -3,5 +3,14 @@
 int ft_printf(const char *format, ...)
 
 {
+    va_list args;
+    int i;
+    int ret;
 
+    i = -1;
+    va_start(args, format);
+    while (format[i++])
+    {
+        if (format[i]  == '%')
+    }
 }
